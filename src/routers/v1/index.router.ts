aa5f739733fcb7f66cc14085f23690d561fe42ce
Assis-Mohanty <assis.mohanty.98@gmail.com>
2026-03-06 21:26:51 +0530

    added schoolRouter

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import pingRouter from './ping.router';
+import schoolRouter from './school.router';
 
 const v1Router = express.Router();
 
 
 
 v1Router.use('/ping',  pingRouter);
-
+v1Router.use('/school', schoolRouter);
 export default v1Router;
